907ebacb77f2a1c5b3c98026be75d91b993d9c3f
Examtutor <venkatesanr63@gmail.com>
2017-10-07 09:55:22 +0530

    Update return.php

--- return.php
+++ return.php
@@ -17,7 +17,7 @@
 /**
  * PayUMoney.com utility script
  *
- * @package    enrol_authorizedotnet
+ * @package    enrol_payumoneydotcom
  * @copyright  2015 Dualcube, Moumita Ray, Parthajeet Chakraborty
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
